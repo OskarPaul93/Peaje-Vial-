4d8576f15c1b784b1277acf08ebfe30b612ecf72
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-08-15 20:22:16 -0500

    Se crea un paquete y clase Conductor

---
+++ src/com/krakedev/peaje/entidades/Conductor.java
@@ -0,0 +1,53 @@
+package com.krakedev.peaje.entidades;
+
+public class Conductor {
+	
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	
+	
+	public Conductor () {
+		
+	}
+	
+	
+	public Conductor(String cedula, String nombre, String apellido) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	public void imprimir () {
+		String mensaje;
+		mensaje = "Cedula: " + cedula
+				+ " , Nombre : " + nombre
+				+ " , Apellido: " + apellido; 
+				
+		System.out.println(mensaje);
+				
+	}
+	
+
+}
