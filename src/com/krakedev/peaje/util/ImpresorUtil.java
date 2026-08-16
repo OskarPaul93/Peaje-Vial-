b92b875a03ad86813ce8c8737893c4b1e58ff20f
OskarPaul93 <paul_1993oscar@hotmail.com>
2026-08-15 20:56:30 -0500

    Se crea clase ImpresorUtil

---
+++ src/com/krakedev/peaje/util/ImpresorUtil.java
@@ -0,0 +1,21 @@
+package com.krakedev.peaje.util;
+
+import com.krakedev.peaje.entidades.Vehiculo;
+
+public class ImpresorUtil {
+
+	public void imprimirVehiculo(Vehiculo vehiculo) {
+
+		System.out.println("----- VEHICULO -----");
+		System.out.println("Placa: " + vehiculo.getPlaca());
+		System.out.println("Tipo: " + vehiculo.getTipo());
+
+		System.out.println("----- PROPIETARIO -----");
+		vehiculo.getPropietario().imprimir();
+
+		System.out.println("----- TAG ELECTRONICO -----");
+		vehiculo.getTag().imprimir();
+
+		System.out.println("Saldo disponible: " + vehiculo.getTag().getSaldo());
+	}
+}
